7062a968779afa3a84bd0c4a791382bdc3a4e063
yoyanni <yanni.alevras@gmail.com>
2026-03-31 00:00:24 +0300

    Fix auth routes - Add required decorators because of validation pipe

--- apps/backend/src/auth/dto/login.dto.ts
+++ apps/backend/src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
 export class LoginDto {
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
   password: string;
 }
--- apps/backend/src/auth/dto/register.dto.ts
+++ apps/backend/src/auth/dto/register.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class RegisterDto {
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
   password: string;
+
+  @IsString()
   name: string;
 }
