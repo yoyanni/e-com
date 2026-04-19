96f5a0f62db3110547dee1c53d45d5de208b1f2d
yoyanni <yanni.alevras@gmail.com>
2026-04-19 23:45:02 +0300

    Add Login page - Add metadata to page - Implement form component as Client component

--- apps/frontend/api/service.ts
+++ apps/frontend/api/service.ts
@@ -1,5 +1,8 @@
-import { IRegisterDto } from "@e-com/shared";
+import { ILoginDto, IRegisterDto } from "@e-com/shared";
 import apiClient from "./client";
 
 export const registerUser = (dto: IRegisterDto) =>
   apiClient.post("/auth/register", dto);
+
+export const loginUser = (dto: ILoginDto) =>
+  apiClient.post("/auth/login", dto);
