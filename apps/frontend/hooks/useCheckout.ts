b03c49ea510a46cb066ee91ad134a0eec7c08a8b
yoyanni <yanni.alevras@gmail.com>
2026-04-28 12:06:09 +0300

    Implement Orders functionality - Add `/account/...` pages - Add `useOrders` hooks - Add Route Handler `/api/orders/[id]`

--- apps/frontend/hooks/useCheckout.ts
+++ apps/frontend/hooks/useCheckout.ts
@@ -2,8 +2,7 @@ import { useRouter } from "next/navigation";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { checkoutCart } from "@/api/service";
 import { CART_QUERY_KEY } from "./useCart";
-
-export const ORDERS_QUERY_KEY = ["orders"] as const;
+import { ORDERS_QUERY_KEY } from "./useOrders";
 
 export function useCheckout() {
   const queryClient = useQueryClient();
