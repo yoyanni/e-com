20b39810cd130ee051fbb2ed5a7b3b63dc423366
yoyanni <yanni.alevras@gmail.com>
2026-05-05 09:43:36 +0300

    Add CORS details to BE for security reasons

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,6 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /**
+   * Since the FE uses Route Handlers and Server Components, CORS isnt required.
+   * However, it is setup for security reasons.
+   */
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -11,6 +21,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 void bootstrap();
